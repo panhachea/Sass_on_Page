da21bbbb1293e5eb91bd5a38f7b970153da960d1
panhachea <cheapanha04@gmail.com>
2017-08-31 22:47:21 +0700

    Sass_Template

---
+++ Web_demo2_sass/gulpfile.js
@@ -0,0 +1,22 @@
+
+var gulp =require('gulp');
+
+
+var elixir = require('laravel-elixir');
+
+
+elixir(function(mix) {
+    mix.sass('style.scss')
+
+});
+
+elixir(function(mix) {
+    mix.sass('style1.scss')
+
+
+});
+
+
+
+
+
